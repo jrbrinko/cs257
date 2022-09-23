dd5fdfcd1b0f6a681ac8d1ca3e8fe9a78e3cfa7d
Amir Al-Sheikh <amir@Amirs-MacBook-Pro.local>
2022-09-23 17:01:04 -0500

    adding booksdatasource.py

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -1,14 +1,15 @@
 '''
    booksdatasourcetest.py
-   Jeff Ondich, 24 September 2021
+   Amir Al-Sehikh and James Brink
+   23 September 2022
 '''
 
-import booksdatasource
+from booksdatasource import Author, Book, BooksDataSource
 import unittest
 
 class BooksDataSourceTester(unittest.TestCase):
     def setUp(self):
-        self.data_source = booksdatasource.BooksDataSource('books1.csv')
+        self.data_source = BooksDataSource('books1.csv')
 
     def tearDown(self):
         pass
@@ -16,81 +17,96 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_unique_author(self):
         authors = self.data_source.authors('Pratchett')
         self.assertTrue(len(authors) == 1)
-        self.assertTrue(authors[0] == booksdatasource.Author('Pratchett', 'Terry'))
+        self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
-    def test_unique_title(self):
-        titles = self.data_source.books('Beloved')
-        self.assertTrue(len(titles) == 1)
-        self.assertTrue(titles[0] == booksdatasource.Book('Beloved'))
+    def test_unique_books(self):
+        books = self.data_source.books('Emma')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Emma'))
 
-    def test_title_sort(self):
-        titles = self.data_source.books()
-        prev_book = titles[0]
-        for book in titles:
-            self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
-
-    def test_author_search(self):
-        authors = self.data_source.authors("Christie")
-        self.assertEqual(len(authors), 1)
-        self.assertEqual(authors[0], booksdatasource.Author("Christie", "Agatha"))
+    def test_all_books(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books()
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('Emma'))
+        self.assertTrue(books[1] == Book('Neverwhere'))
+        self.assertTrue(books[2] == Book('Omoo'))
 
     def test_all_authors(self):
-        authors = self.data_source.authors()
-        self.assertEqual(len(authors), 22)
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        authors = tiny_data_source.books()
+        self.assertTrue(len(authors) == 3)
+        self.assertTrue(authors[0] == Author('Gaiman', 'Neil'))
+        self.assertTrue(authors[1] == Author('Austen', 'Jane'))
+        self.assertTrue(authors[2] == Author('Melville', 'Herman'))
 
-    def test_author_same_surname(self):
-        authors = self.data_source.authors("Bront")
-        self.assertEqual(len(authors), 3)
-        self.assertEqual(authors[0], booksdatasource.Author("Brontë", "Ann"))
-        self.assertEqual(authors[1], booksdatasource.Author("Brontë", "Charlotte"))
-        self.assertEqual(authors[2], booksdatasource.Author("Brontë", "Emily"))
+    def test_author_tiebreak(self):
+        authors = self.data_source.authors('bront')
+        self.assertTrue(authors[0] == Author('Brontë', 'Ann'))
+        self.assertTrue(authors[1] == Author('Brontë', 'Charlotte'))
+        self.assertTrue(authors[2]== Author('Brontë', 'Emily'))
 
-    def test_default_sort(self):
-        titles = self.data_source.books(None, "random_thing")
-        prev_book = titles[0]
-        for book in titles:
-            self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
+    def test_no_author(self):
+        authors = self.data_source.authors('zzzzzzzzz')
+        self.assertTrue(len(authors) == 0)
 
-    def test_between_year_sort(self):
-        years = self.data_source.books_between_years(1937, 2010)
-        prev_book = years[0]
-        for book in years:
-            self.assertLessEqual(book.publication_year,2010)
-            self.assertGreaterEqual(book.publication_year,1937)
-            self.assertLessEqual(prev_book.publication_year,book.publication_year)
-            if(prev_book.publication_year == book.publication_year):
-                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
+    def test_no_books(self):
+        books = self.data_source.books('zzzzzzzzz')
+        self.assertTrue(len(books) == 0)
 
-    def test_year_sort(self):
-        years = self.data_source.books()
-        prev_book = years[0]
-        for book in years:
-            if(prev_book.publication_year == book.publication_year):
-                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
+    def test_author_sort(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        authors = tiny_data_source.authors()
+        self.assertTrue(len(authors) == 3)
+        self.assertTrue(authors[0] == Author('Austen','Jane'))
+    
+    def test_book_sort_title(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books()
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('Emma'))
 
-    def test_start_year_only_sort(self):
-        years = self.data_source.books_between_years(start_year=1939)
-        prev_book = years[0]
-        for book in years:
-            self.assertGreaterEqual(book.publication_year,1939)
-            self.assertLessEqual(prev_book.publication_year,book.publication_year)
-            if(prev_book.publication_year == book.publication_year):
-                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
+    def test_book_sort_year_tiebreak(self):
+        books = self.data_source.books(None, year)
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('Emma'))
 
-    def test_end_year_only_sort(self):
-        years = self.data_source.books_between_years(end_year=2010)
-        prev_book = years[0]
-        for book in years:
-            self.assertLessEqual(book.publication_year,2010)
-            self.assertLessEqual(prev_book.publication_year,book.publication_year)
-            if(prev_book.publication_year == book.publication_year):
-                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
-            prev_book = book
+    def test_book_sort_year_tiebreak(self):
+        tiebreak_data_source = BooksDataSource('tiebreak.csv')
+        books = tiebreak_data_source.books(None, year)
+        self.assertTrue(books[4] == Book('All Clear'))
+        self.assertTrue(books[5] == Book('Blackout'))
+        
+    def test_book_sort_year(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books()
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('Emma'))
+
+    def test_year_search(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books_between_years(None, 1900)
+        self.assertTrue(len(books) == 2)
+        self.assertTrue(books[0] == Book(("Emma")))
+
+    def test_year_search_no_params(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books_between_years()
+        self.assertTrue(len(books) == 3)
+         
+    def test_year_between_two_params(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books_between = tiny_data_source.books_between_years(1847, 1996)
+        self.assertTrue(books_between[0] == Book('Emma'))
+        self.assertTrue(books_between[1] == Book('Neverwhere'))
+        self.assertTrue(books_between[2] == Book('Omoo'))
+        
+    def test_year_between_no_books(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books_between = tiny_data_source.books_between_years(3000, 4000)
+        self.assertTrue(len(books_between) == 0)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
